8ceb4667c6624825a68c514b7d8d855adf8509da
Mihaly Sisak <sisak.mihaly@gmail.com>
2025-10-03 10:47:16 +0200

    auto generate utf8.dat

--- runtests_luau.sh
+++ runtests_luau.sh
@@ -8,5 +8,8 @@ cmake -G Ninja -B _build -S . -DUSE_INTERNAL_FPCONV=ON -DUSE_LUAU=ON -DCOMPILE_L
         -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build _build --config RelWithDebInfo
 cd tests
+if [ ! -f utf8.dat ]; then
+    ./genutf8.pl
+fi
 ../_build/luau_test luau_test.lua
 
